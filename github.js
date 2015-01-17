6f678c0c2019ae7563b5ba6b4c3a1694965c5f0b
Renante Entera <renantee@zylun.com>
2015-01-17 14:30:30 +0800

    Basic Usage
    - controllers
    - directives and views
    - services (github)
    - routing

---
+++ github.js
@@ -0,0 +1,43 @@
+(function() {
+	var app = angular.module("githubViewer");
+
+	var github = function($http) {
+		var getUser = function(username) {
+			return $http.get("https://api.github.com/users/" + username)
+						.then(function(response) {
+							return response.data;
+						});
+		};
+
+		var getRepos = function(user) {
+			return $http.get(user.repos_url)
+						.then(function(response) {
+							return response.data;
+						});
+		};
+
+		var getRepoDetails = function(username, reponame) {
+			var repo;
+			var repoUrl = "https://api.github.com/repos/" + username + "/" + reponame;
+
+			return $http.get(repoUrl)
+						.then(function(response) {
+							repo = response.data;
+							return $http.get(repoUrl + "/contributors");
+						})
+						.then(function(response) {
+							repo.contributors = response.data;
+							return repo;
+						})
+		};
+
+		return {
+			getUser: getUser,
+			getRepos: getRepos,
+			getRepoDetails: getRepoDetails
+		};
+	};
+
+	
+	app.factory("github", github);
+}());
